9d0193e476795255782f7c325523c56ec945fac7
arpan007 <arpansoni0007@gmail.com>
2019-07-14 11:45:25 +0530

    Create Model -a

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\review;
+use Faker\Generator as Faker;
+
+$factory->define(review::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
